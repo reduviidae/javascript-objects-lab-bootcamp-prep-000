95870373f59cda5951b1df4df306c0a28d84d092
Wil Cook <wil.cook.is@gmail.com>
2018-10-19 13:41:32 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -10,6 +10,7 @@ function destructivelyUpdateObjectWithKeyAndValue(object, key, value) {
 }
 
 function deleteFromObjectByKey(object, key) {
-  const newObject = {};
-  
+  const newObject = Object.assign({}, object);
+  delete newObject.key;
+  return newObject;
 }
